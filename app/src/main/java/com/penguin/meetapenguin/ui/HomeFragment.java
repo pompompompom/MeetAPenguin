c84b719987213dca897f45fa23729a550be562e7
mitayun <mitayun@gmail.com>
2016-04-02 11:07:34 -0700

    Initial commit

---
+++ app/src/main/java/com/penguin/meetapenguin/ui/HomeFragment.java
@@ -0,0 +1,22 @@
+package com.penguin.meetapenguin.ui;
+
+import com.penguin.meetapenguin.R;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Fragment to display main screen.
+ */
+public class HomeFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+        return v;
+    }
+}
