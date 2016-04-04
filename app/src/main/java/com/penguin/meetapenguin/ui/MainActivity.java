5d36dd13d74a47179414ca920453ddc1dd3077df
mitayun <mitayun@gmail.com>
2016-04-04 13:07:42 -0700

    Add settings fragment

--- app/src/main/java/com/penguin/meetapenguin/ui/MainActivity.java
+++ app/src/main/java/com/penguin/meetapenguin/ui/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener, ContactListFragment.OnListFragmentInteractionListener, PrepareShareFragment.OnShareFragmentInteraction {
 
     private Fragment homeFragment;
+    private Fragment settingsFragment = new SettingsFragment();
     private Fragment contactFragment = new ContactListFragment();
     private Fragment shareFragment = new PrepareShareFragment();
     private DrawerLayout mDrawer;
@@ -82,6 +83,7 @@ public class MainActivity extends AppCompatActivity
         if (id == R.id.nav_home) {
             displayFragment(homeFragment, title);
         } else if (id == R.id.nav_settings) {
+            displayFragment(settingsFragment, title);
         } else if (id == R.id.nav_contacts) {
             displayFragment(contactFragment, title);
         } else if (id == R.id.share) {
