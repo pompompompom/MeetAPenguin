8ad58dd74747415beeaab354fbd69c78043ba0f9
Prin Oungpasuk <poungpas@andrew.cmu.edu>
2016-04-04 23:49:16 -0400

    Added save button to home fragment

--- app/src/main/java/com/penguin/meetapenguin/ui/HomeFragment.java
+++ app/src/main/java/com/penguin/meetapenguin/ui/HomeFragment.java
@@ -17,6 +17,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import android.support.v7.widget.Toolbar;
 
@@ -94,6 +95,22 @@ public class HomeFragment extends Fragment {
         ContactViewAdapter contactAdapter = new ContactViewAdapter(contact.getContactInfoArrayList(),
                 null, getContext(), ContactViewAdapter.MODE_EDIT_CONTACT);
         recyclerView.setAdapter(contactAdapter);
+
+        Button saveButton = (Button) v.findViewById(R.id.save_button);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AlertDialog.Builder(inflater.getContext()).setMessage
+                        ("Changes saved!")
+                        .setPositiveButton("Okay", new
+                                DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+
+                                    }
+                                }).show();
+            }
+        });
 
         return v;
     }
