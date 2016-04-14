3a3348420024d4768c32f6cd90a8229d3fe73001
urbano <oximer@gmail.com>
2016-04-13 20:58:03 -0700

    Creating the functions to access the API.

--- app/src/main/java/com/penguin/meetapenguin/ws/remote/RetrieveEntityRequest.java
+++ app/src/main/java/com/penguin/meetapenguin/ws/remote/RetrieveEntityRequest.java
@@ -13,14 +13,14 @@ import java.io.UnsupportedEncodingException;
 import java.util.Map;
 
 
-public class EntityRequest<T> extends Request<T> {
+public class RetrieveEntityRequest<T> extends Request<T> {
     private final Gson gson = new Gson();
     private final Class<T> clazz;
     private final Map<String, String> headers;
     private final Response.Listener<T> listener;
 
-    public EntityRequest(String url, Class<T> clazz, Map<String, String> headers,
-                         Response.Listener<T> listener, Response.ErrorListener errorListener) {
+    public RetrieveEntityRequest(String url, Class<T> clazz, Map<String, String> headers,
+                                 Response.Listener<T> listener, Response.ErrorListener errorListener) {
         super(Method.GET, url, errorListener);
         this.clazz = clazz;
         this.headers = headers;
