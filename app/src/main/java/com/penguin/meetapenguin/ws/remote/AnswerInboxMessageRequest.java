c250fdfa2aec04c1d310c747c9d5a9cbff7ac2a6
urbano <oximer@gmail.com>
2016-04-22 17:22:27 -0700

    Working on the accepting a message renew

--- app/src/main/java/com/penguin/meetapenguin/ws/remote/AnswerInboxMessageRequest.java
+++ app/src/main/java/com/penguin/meetapenguin/ws/remote/AnswerInboxMessageRequest.java
@@ -3,70 +3,53 @@ package com.penguin.meetapenguin.ws.remote;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.NetworkResponse;
-import com.android.volley.ParseError;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.toolbox.HttpHeaderParser;
 import com.penguin.meetapenguin.entities.InboxMessage;
 import com.penguin.meetapenguin.util.ProfileManager;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
  * Created by urbano on 4/22/16.
  */
-public class AnswerInboxMessageRequest extends Request<JSONObject> {
+public class AnswerInboxMessageRequest extends Request<String> {
 
 
-    private static final String URL = "http://10.0.3.2:8080/rest";
+    private static final String URL = "http://10.1.10.26:8080/rest";
     private final Response.Listener<String> mListener;
-    private final boolean mStatus;
+    Map<String, String> mParams;
 
     public AnswerInboxMessageRequest(InboxMessage message, boolean status, Response.Listener<String> listener, Response.ErrorListener errorListener) {
-        super(Method.POST, String.format("%s/messages/%s/status/", URL, message.getId()), errorListener);
+        super(Method.POST, String.format("%s/messages/%s/status", URL, message.getId()), errorListener);
         mListener = listener;
-        mStatus = status;
+        mParams = new HashMap<String, String>();
+        mParams.put("accepted", String.valueOf(status));
     }
 
     @Override
     public Map<String, String> getParams() {
-        Map<String, String> parameters = new HashMap<String, String>();
-        parameters.put("status", String.valueOf(mStatus));
-        return parameters;
+        return mParams;
     }
 
     @Override
-    protected Response<JSONObject> parseNetworkResponse(NetworkResponse response) {
-        try {
-            String jsonString = new String(response.data,
-                    HttpHeaderParser.parseCharset(response.headers));
-
-            return Response.success(new JSONObject(jsonString),
-                    HttpHeaderParser.parseCacheHeaders(response));
-
-        } catch (UnsupportedEncodingException e) {
-            return Response.error(new ParseError(e));
-        } catch (JSONException je) {
-            return Response.error(new ParseError(je));
-        }
+    protected Response<String> parseNetworkResponse(NetworkResponse response) {
+        String jsonString = new String(response.data);
+        return Response.success(jsonString,
+                HttpHeaderParser.parseCacheHeaders(response));
     }
 
     @Override
     public Map<String, String> getHeaders() throws AuthFailureError {
         Map<String, String> params = new HashMap<String, String>();
-        params.put("Content-Type", "application/json");
-        params.put("Accept", "application/json");
         params.put("userId", String.valueOf(ProfileManager.getInstance().getUserId()));
         return params;
     }
 
     @Override
-    protected void deliverResponse(JSONObject response) {
+    protected void deliverResponse(String response) {
         mListener.onResponse(response.toString());
     }
 
